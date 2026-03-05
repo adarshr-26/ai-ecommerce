22e18f2d4a594b2b78bf9436d1cf7237bb66f35b
adarshr-26 <adarshr2624@gmail.com>
2026-03-05 16:33:03 +0530

    added homepage route

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("AI Ecommerce Website is Working 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('store.urls')),
-
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', home),
 ]
