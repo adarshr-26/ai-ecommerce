49ee503c4d258c4ab930beb5b85c4d656bc175e1
adarshr-26 <adarshr2624@gmail.com>
2026-03-05 12:17:45 +0530

    fixed namespace

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('order/<int:product_id>/', views.place_order, name='place_order'),
